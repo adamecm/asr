33874f56e84c7f5f1f389d4800d418d89e11bcc6
adamecm <martinadamec2000@gmail.com>
2022-12-20 14:09:23 +0100

    funguje

--- decoding/viterbi.py
+++ decoding/viterbi.py
@@ -23,15 +23,6 @@ with open("./00170005_14.txt","r",encoding="windows-1250") as file:
   for line in file.readlines():
     acoustic_model.append([-np.log10(float(x)) for x in line.split()])
 
-# a = []
-# for i in range(38):
-#   for j in range(38):
-#     #print(i,j, acoustic_model[0][i]+trans_probs[i][0]+acoustic_model[1][j]+trans_probs[j][1])
-#     a.append((i,j, acoustic_model[0][i]+trans_probs[i][0]+acoustic_model[1][j]+trans_probs[j][0]))
-
-# print(sorted(a,key=lambda x: x[2]))
-# exit()
-
 #####init#####
 phi_net = copy.deepcopy(word_net)
 # min_path = np.infty
@@ -41,68 +32,32 @@ for w in range(len(phi_net)):
   for phoneme_idx in range(1,len(phi_net[w])):
     phi_net[w][phoneme_idx] = np.infty
 
-old_phi_net = copy.deepcopy(phi_net)
-ends = [x[-1] for x in phi_net]
-min_path = np.argmin(ends)
-min_state = word_net[min_path][-1]
-min_val = ends[min_path] + trans_probs[min_state][1]
-##### calc #####
-min_cost = []
 
 
 print("t=1 a(a) = ", phi_net[0][0])
 print("t=1 a(aby) = ", phi_net[1][0])
 print("t=1 # = ", phi_net[-1][0]) 
 
-
+tokens = [np.zeros(len(x)) for x in phi_net]
 for t in range(1,len(acoustic_model)): #t = 2 to T (392), every vector from acoustic model
-  
+  old_phi_net = copy.deepcopy(phi_net)
+
+  ends = [(x[-1] + trans_probs[y[-1]][1]) for x,y in zip(old_phi_net,word_net)]
+  min_val = min(ends)
+
+
   for w in range(len(phi_net)): #every word
     word = word_net[w]
+    phi_net[w][0] = min(min_val,old_phi_net[w][0]+trans_probs[word[0]][0])+acoustic_model[t][word[0]]
     #1 to end of each word
-    for j in range(len(word)):
-      temp = phi_net[w][j]
+    for j in range(1,len(word)):
 
-      prev_char = word[j-1]
-      current_char = word[j]
-
-      prev_phi = old_phi_net[w][j-1] + trans_probs[prev_char][1] #prev + trans
-      current_phi = old_phi_net[w][j] + trans_probs[current_char][0] #same + loop
+      prev_phi = old_phi_net[w][j-1] + trans_probs[word[j-1]][1] #prev + trans
+      current_phi = old_phi_net[w][j] + trans_probs[word[j]][0] #same + loop
     
-      phi_net[w][j] = min(prev_phi,current_phi) + acoustic_model[t][current_char]
-      old_phi_net[w][j] = temp
-      # old_phi_net[w][j] = phi_net[w][j]
-    # ends.append(phi_net[w][-1])
+      phi_net[w][j] = min(prev_phi,current_phi) + acoustic_model[t][word[j]]
+  
     
-  
-  for w in range(len(phi_net)):
-    temp = phi_net[w][0]
-
-    prev_char = word_net[min_path][-1]
-    current_char = word_net[w][0]
-
-    prev_phi = old_phi_net[min_path][-1] + trans_probs[prev_char][1]
-    # prev_phi = min_val + trans_probs[prev_char][1]
-    current_phi = old_phi_net[w][0] + trans_probs[current_char][0]
-    phi_net[w][0] = min(prev_phi,current_phi) + acoustic_model[t][current_char]
-
-    old_phi_net[w][0] = temp
-#  #first of each word
-# for w in range(len(phi_net)):
-  
-  ends = [(x[-1] + trans_probs[y[-1]][1]) for x,y in zip(old_phi_net,word_net)]
-  # ends = [x[-1] for x in old_phi_net]
-  min_path = np.argmin(ends)
-  min_state = word_net[min_path][-1]
-  min_val = ends[min_path]# + trans_probs[min_state][1]
-  # min_val = old_phi_net[min_path][-1] + trans_probs[min_state][0] + acoustic_model[t][min_state]
-
-
-  
-  
-
- 
-
 
 
   if t == 1:
@@ -113,37 +68,10 @@ for t in range(1,len(acoustic_model)): #t = 2 to T (392), every vector from acou
     print("t=392 a(a) = ", phi_net[0][0])
     # print("t=392 a(aby) = ", phi_net[1][0])
     print("t=392 # = ", phi_net[-1][0])
-final_min_val = min([x[-1] for x in phi_net])
+
+
+ends = [(x[-1] + trans_probs[y[-1]][1]) for x,y in zip(phi_net,word_net)]
+final_min_val = min(ends)
 print(final_min_val)
 
-print(min_val)
 
-
-"""
-list pravdepodobnosti
-
-uz je vsechno jako -log10 => je treba pricitat a ne odcitat
-
-min( phi_{j-1}(t-1)+A(j-1,j), phi_{j}(t-1)+A(j,j))
-
-
-pres vsechna t (radek souboru)
-  pres vsechna slova
-    vypocet koncu (ty delsi kraty netreba pocitat)
-
-  pres vsechna slova
-    pres vsechny fonemy
-      if zacatek
-        koncova predchozi ppst
-      else
-        predchozi ppst
-
-      vzorec ze souboru
-
-
-
-
-for t in range(len(acoustic_model)):
-  [print(i,x) for i,x in enumerate()]
-
-"""
